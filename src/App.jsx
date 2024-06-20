161e6f06053f37c6c5b298f2315fb28add3d7506
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-20 18:36:19 +0530

    Added Grid-Virtualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,78 @@
 import VirtualizedGrid from "./components/VirtualizedGrid";
 import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+const LIMIT = 5;
 
 const App = () => {
-	const items = Array.from({ length: 1000 }, (_, index) => `Item ${index + 1}`);
+  const [items, setItems] = useState({
+    itemsList: [],
+    isLoading: true,
+    hasMore: true,
+  });
+  const { itemsList, isLoading, hasMore } = items;
+  const countRef = useRef(LIMIT);
 
-	const renderItem = (item) => {
-		console.log("in render", item);
-		return (
-			<div style={{ padding: "20px", border: "1px solid #ccc" }}>{item}</div>
-		);
-	};
+  const loadMore = () => {
+    if (isLoading || !hasMore) {
+      return;
+    }
+    setItems((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    setTimeout(() => {
+      const newArr = Array.from({ length: LIMIT }, (_, index) => ({
+        id: index + countRef.current,
+      }));
+      setItems((prev) => ({
+        ...prev,
+        isLoading: false,
+        hasMore: prev.itemsList.length < 60,
+        itemsList: [...prev.itemsList, ...newArr],
+      }));
+      countRef.current += LIMIT;
+    }, [2000]);
+  };
 
-	const gridStyles = {
-		display: "grid",
-		gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
-		gap: "10px",
-		padding: "10px",
-	};
+  const renderItem = (item) => {
+    return (
+      <div style={{ padding: "20px", border: "1px solid #ccc" }}>{item}</div>
+    );
+  };
 
-	return (
-		<div style={{ height: "100vh", width: "100%" }}>
-			<VirtualizedGrid
-				items={items}
-				renderItem={renderItem}
-				gridStyles={gridStyles}
-			/>
-		</div>
-	);
+  const gridStyles = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+    gap: "10px",
+    padding: "10px",
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setItems((prev) => ({
+        ...prev,
+        itemsList: Array.from({ length: LIMIT }, (_, index) => ({
+          id: index,
+        })),
+        hasMore: true,
+        isLoading: false,
+      }));
+    }, [2000]);
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <VirtualizedGrid
+        items={itemsList}
+        renderItem={renderItem}
+        gridStyles={gridStyles}
+        loadMore={loadMore}
+        hasMore={hasMore}
+        isLoading={isLoading}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/VirtualizedGrid.jsx
+++ src/components/VirtualizedGrid.jsx
@@ -1,98 +1,187 @@
+import PropTypes from "prop-types";
 import { useState, useRef, useEffect, useCallback } from "react";
 
-const VirtualizedGrid = ({ items, renderItem, gridStyles }) => {
-	const [visibleItems, setVisibleItems] = useState([]);
-	const [itemDimensions, setItemDimensions] = useState({ width: 0, height: 0 });
-	const gridRef = useRef(null);
-	const measureRef = useRef(null);
+const VirtualizedGrid = ({
+  items,
+  renderItem,
+  gridStyles,
+  loadMore,
+  hasMore,
+  isLoading,
+}) => {
+  const [visibleItems, setVisibleItems] = useState({
+    list: [],
+    offsetTop: 0,
+    offsetBottom: 0,
+    lastRowIndex: 0,
+  });
+  const [itemDimensions, setItemDimensions] = useState({ width: 0, height: 0 });
+  const gridRef = useRef(null);
+  const measureRef = useRef(null);
+  const lastElementRef = useRef(null);
+  const timeoutRef = useRef(null);
 
-	const measureItemDimensions = () => {
-		const { width, height } = measureRef.current.getBoundingClientRect();
-		setItemDimensions({ width, height });
-		console.log("first", width, height);
-	};
+  const measureItemDimensions = () => {
+    if (!measureRef.current) {
+      return;
+    }
+    const { width, height } = measureRef.current.getBoundingClientRect();
+    setItemDimensions({ width, height });
+  };
 
-	const calculateVisibleItems = useCallback(() => {
-		const { scrollTop, clientHeight, clientWidth } = gridRef.current;
+  const calculateVisibleItems = useCallback(() => {
+    const { scrollTop, clientHeight, clientWidth } = gridRef.current;
 
-		console.log("first", scrollTop, clientHeight, clientWidth);
+    if (itemDimensions.height === 0 || itemDimensions.width === 0) {
+      return;
+    }
 
-		if (itemDimensions.height === 0 || itemDimensions.width === 0) {
-			return;
-		}
+    const columns = Math.ceil(clientWidth / itemDimensions.width);
+    const rows = Math.ceil(clientHeight / itemDimensions.height) + 1;
 
-		const columns = Math.floor(clientWidth / itemDimensions.width);
-		const rows = Math.ceil(clientHeight / itemDimensions.height);
-		console.log("rows", rows);
-		console.log("columns", columns);
-		const startRow = Math.floor(scrollTop / itemDimensions.height);
+    const lastPossibleRowIndex = Math.ceil(items.length / columns) - 1;
 
-		const firstItemIdx = startRow * columns;
-		const lastItemIdx = firstItemIdx + rows * columns;
-		console.log("=============", firstItemIdx, lastItemIdx);
-		const visibleItems = items.slice(firstItemIdx, lastItemIdx);
-		console.log("sorted", visibleItems);
-		setVisibleItems(visibleItems);
-	}, [itemDimensions.height, itemDimensions.width, items]);
+    const startRowIdx =
+      Math.floor(scrollTop / itemDimensions.height) - 1 < 0
+        ? 0
+        : Math.floor(scrollTop / itemDimensions.height) - 1;
 
-	const handleScroll = () => {
-		calculateVisibleItems();
-	};
+    const endRowIdx =
+      Math.ceil(items.length / columns) <= startRowIdx + rows
+        ? Math.ceil(items.length / columns)
+        : startRowIdx + rows;
 
-	useEffect(() => {
-		measureItemDimensions();
-		window.addEventListener("resize", measureItemDimensions);
-		window.addEventListener("resize", calculateVisibleItems);
-		return () => {
-			window.removeEventListener("resize", measureItemDimensions);
-			window.removeEventListener("resize", calculateVisibleItems);
-		};
-	}, [calculateVisibleItems]);
+    const firstItemIdx = startRowIdx * columns;
+    const lastItemIdx = (endRowIdx + 1) * columns - 1;
 
-	useEffect(() => {
-		calculateVisibleItems();
-	}, [calculateVisibleItems, itemDimensions]);
+    const visibleItems = items.slice(firstItemIdx, lastItemIdx + 1); // + 1 because slice exclude last index
 
-	return (
-		<>
-			<div
-				style={{
-					position: "absolute",
-					height: "auto",
-					width: "100%",
-					visibility: "hidden",
-					...gridStyles,
-				}}
-			>
-				<div ref={measureRef} className="dummyDiv">
-					{renderItem(items[0])}
-				</div>
-			</div>
-			<div
-				ref={gridRef}
-				style={{
-					overflowY: "auto",
-					height: "100%",
-					...gridStyles,
-					gridAutoRows: `${itemDimensions.height}px`,
-				}}
-				onScroll={handleScroll}
-			>
-				{/* <div
-					style={{
-						// display: "grid",
-						...gridStyles,
-						// gridAutoRows: `minmax(${itemDimensions.height}px, auto)`,
-					}}
-				> */}
-				{visibleItems.length > 0 &&
-					visibleItems.map((item, index) => (
-						<div key={index}>{renderItem(item)}</div>
-					))}
-				{/* </div> */}
-			</div>
-		</>
-	);
+    setVisibleItems((prevVisibleItems) => {
+      const offsetTop =
+        startRowIdx * itemDimensions.height + parseInt(gridStyles.padding);
+      const lastRowIndex =
+        prevVisibleItems.lastRowIndex < endRowIdx
+          ? endRowIdx
+          : prevVisibleItems.lastRowIndex;
+      const offsetBottom =
+        (lastPossibleRowIndex - endRowIdx) * itemDimensions.height +
+        parseInt(gridStyles.padding);
+      return {
+        list: visibleItems,
+        offsetTop,
+        offsetBottom,
+        lastRowIndex,
+      };
+    });
+  }, [gridStyles.padding, itemDimensions.height, itemDimensions.width, items]);
+
+  const handleScroll = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    if (isLoading && !hasMore) {
+      return;
+    }
+    timeoutRef.current = setTimeout(() => {
+      calculateVisibleItems();
+    }, 100);
+  };
+
+  useEffect(() => {
+    if (isLoading && !hasMore) {
+      return;
+    }
+    if (lastElementRef && lastElementRef.current) {
+      const observer = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          loadMore();
+        }
+      });
+      const element = lastElementRef.current;
+      observer.observe(element);
+
+      return () => {
+        if (element) observer.unobserve(element);
+      };
+    }
+  }, [hasMore, isLoading, loadMore, visibleItems]);
+
+  useEffect(() => {
+    measureItemDimensions();
+    window.addEventListener("resize", measureItemDimensions);
+    window.addEventListener("resize", calculateVisibleItems);
+    return () => {
+      window.removeEventListener("resize", measureItemDimensions);
+      window.removeEventListener("resize", calculateVisibleItems);
+    };
+  }, [calculateVisibleItems]);
+
+  useEffect(() => {
+    calculateVisibleItems();
+  }, [calculateVisibleItems, itemDimensions]);
+
+  return (
+    <>
+      {items.length > 0 && (
+        <div
+          style={{
+            position: "absolute",
+            height: "auto",
+            width: "100%",
+            visibility: "hidden",
+            ...gridStyles,
+          }}
+        >
+          <div ref={measureRef} className="dummyDiv">
+            {renderItem(items[0].id)}
+          </div>
+        </div>
+      )}
+
+      <div
+        ref={gridRef}
+        style={{
+          overflowY: "auto",
+          height: "100%",
+          width: "100%",
+        }}
+        onScroll={handleScroll}
+      >
+        <div
+          style={{
+            ...gridStyles,
+            gridAutoRows: `${itemDimensions.height}px`,
+            paddingTop: visibleItems.offsetTop,
+            paddingBottom: visibleItems.offsetBottom,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          {visibleItems.list.length > 0 &&
+            visibleItems.list.map((item) => (
+              <div
+                ref={item.id === items.at(-1).id ? lastElementRef : null}
+                key={item.id}
+              >
+                {renderItem(item.id)}
+              </div>
+            ))}
+          {isLoading &&
+            hasMore &&
+            Array.from({ length: 5 }, (_, idx) => idx).map((_, index) => (
+              <div key={index} style={{ background: "#F1F1F1" }} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+VirtualizedGrid.propTypes = {
+  items: PropTypes.array,
+  renderItem: PropTypes.func,
+  gridStyles: PropTypes.object,
+  loadMore: PropTypes.func,
 };
 
 export default VirtualizedGrid;
